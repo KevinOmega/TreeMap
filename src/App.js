76a7256a3b482b38eb643474536ac2f19b021d1e
Kevin Huayllas <KevinOmega01@gmail.com>
2023-08-24 10:52:48 -0400

    adding fetching function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./app.css";
+import { links } from "./data";
+import * as d3 from "d3";
 
 const App = () => {
   const { category } = useParams();
+  const [fetched, setFetched] = useState(false);
+  const [data, setData] = useState({});
+
+  const fetchData = useCallback(() => {
+    let url = links.filter((d) => d.name === category)[0].link;
+    d3.json(url).then((value) => setData(value));
+  }, [category]);
+
+  const drawTreeMap = () => {
+    const svg = d3
+      .select(".app-center")
+      .append("svg")
+      .attr("width", "100%")
+      .style("background", "white");
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  useEffect(() => {
+    if (data.name) {
+      drawTreeMap();
+    }
+  }, [data]);
 
   return (
     <div className="app">
